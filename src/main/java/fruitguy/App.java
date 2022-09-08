64f686762670a9b50fe003f4a344477a2bb18d7d
dhopz <david_houpapa@hotmail.com>
2022-09-08 11:29:57 +0100

    scrolling text solution

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -11,7 +11,8 @@ public class App {
 //        System.out.println(Arrays.toString(removeRotten(rotten)));
 //        System.out.println(Arrays.toString(bestSolution(rotten)));
 
-        System.out.println(generateShape(7));
+//        System.out.println(generateShape(7));
+        System.out.println(Arrays.toString(scrollingText("codewars")));
     }
 
     public static String[] removeRotten(String[] fruitBasket){
@@ -42,4 +43,36 @@ public class App {
 
         return a.repeat(n-1) + "+".repeat(n);
     }
+    //"ODEWARSC"
+    public static String[] scrollingText(String word) {
+        String text = word.toUpperCase();
+        ArrayList<String> textVariables = new ArrayList<>();
+        textVariables.add(text);
+        ArrayList<Character> usedLetters = new ArrayList<>();
+        for (int i = 0; i < text.length()-1; i++) {
+            usedLetters.add(text.charAt(i));
+            textVariables.add(text.substring(i+1)+letterCombo(usedLetters).toUpperCase());
+        }
+        return textVariables.toArray(new String[0]);
+    }
+    public static String letterCombo(List<Character> usedLetters){
+        StringBuilder builder = new StringBuilder(usedLetters.size());
+        for(Character ch: usedLetters){
+            builder.append(ch);
+        }
+        return builder.toString();
+    }
+    public static String[] scrollingTextSolution(String text) {
+        String[] result = new String[text.length()];
+        StringBuilder stringBuilder = new StringBuilder(text.toUpperCase());
+        for (int i = 0; i < result.length; i++) {
+            result[i] = stringBuilder.toString();
+            stringBuilder.append(stringBuilder.charAt(0)).deleteCharAt(0);
+        }
+        return result;
+    }
+
+
+
+
 }
