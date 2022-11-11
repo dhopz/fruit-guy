cd614c7f1369bc131ecc30cb86dcd3900ba129d9
dhopz <david_houpapa@hotmail.com>
2022-11-11 16:48:09 +0000

    scoring test add

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -7,9 +7,12 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import static fruitguy.Katas.ScoringTest.sol;
+
 public class App {
     public static void main(String[] args) {
-
+        int[] arr = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        System.out.println(sol(arr,3,-1,2));
 
     }
 
