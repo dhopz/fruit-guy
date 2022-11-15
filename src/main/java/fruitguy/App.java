c9fb3f012bb2374a901539a170ce291ec80e0295
dhopz <david_houpapa@hotmail.com>
2022-11-15 14:14:33 +0000

    removing characters

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -2,15 +2,20 @@ package fruitguy;
 
 //import java.util.*;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static fruitguy.Katas.ReverseString.reverseWords;
+import static fruitguy.Katas.Stratholme.purify;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(reverseWords("double  spaced  words"));
-        System.out.println(reverseWords("double spaced words"));
-        System.out.println(reverseWords("   "));
+        System.out.println(purify("STRING"));
 
     }
+
+
 
 
 
