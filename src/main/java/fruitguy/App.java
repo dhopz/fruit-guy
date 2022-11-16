6850fa87eda20fa69e2c9e72847bdfd41c761e71
dhopz <david_houpapa@hotmail.com>
2022-11-16 17:04:15 +0000

    removing fives

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -13,9 +13,20 @@ import static fruitguy.Katas.Stratholme.purify;
 
 public class App {
     public static void main(String[] args) {
-        String[] cards = new String[]{"AS", "3D", "9S", "KS", "4S"};
-        System.out.println(checkIfFlush(cards));
+        System.out.println(dontGiveMeFive(4,17));
 
+    }
+
+    public static int dontGiveMeFive(int start, int end){
+        ArrayList<Integer> numbers = new ArrayList<>();
+
+        for (int i = start; i <= end; i++){
+            if (!String.valueOf(i).contains("5")){
+                numbers.add(i);
+            }
+        }
+
+        return numbers.size();
     }
 }
 
