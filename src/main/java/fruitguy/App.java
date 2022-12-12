34d844e140b934f87cbeab9fb09ba64748ca5776
dhopz <david_houpapa@hotmail.com>
2022-12-12 11:01:19 +0000

    caffeineBuzz kata

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -9,11 +9,25 @@ import static fruitguy.Katas.DescendingOrderDigit.sortDesc;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(sortDesc(42145));
+//        System.out.println(sortDesc(42145));
+        System.out.println(caffeineBuzz(1));
 
     }
 
+    public static String caffeineBuzz(int n){
+        System.out.println(n);
+        if(n % 3 == 0 && n % 4 == 0){
+            return "CoffeeSc";
+        } else if (n % 3 == 0 && n % 2 == 0) {
+            return "JavaScript";
+        } else if (n % 3 == 0) {
+            return "Java";
+        } else {
+            return "mocha_missing!";
+        }
 
+    }
 }
+
 
 
