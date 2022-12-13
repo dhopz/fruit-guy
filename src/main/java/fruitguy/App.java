8bc8b0c482153a6e6155f87cbc899d4d5f435e92
dhopz <david_houpapa@hotmail.com>
2022-12-13 16:52:16 +0000

    fighter health

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -1,5 +1,7 @@
 package fruitguy;
 
+import fruitguy.Katas.DecWinner.Fighter;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
@@ -10,10 +12,23 @@ import static fruitguy.Katas.DriversLicence.driver;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(driver(new String[]{"Johanna", "","Gibbs", "13-Dec-1981","F"}));
-        System.out.println("GIBBS862131J99AA");
+        Fighter fighter1 = new Fighter("Lew",10,2);
+        Fighter fighter2 = new Fighter("Harry",5,4);
+        System.out.println(declareWinner(fighter1,fighter2,"Lew"));
 
+    }
 
+    public static String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+
+        if (Objects.equals(firstAttacker, fighter1.name)){
+            fighter2.health = fighter2.health - fighter1.damagePerAttack;
+        } else {
+            fighter1.health = fighter1.health - fighter2.damagePerAttack;
+        }
+        System.out.println("Fighter2 health " + fighter2.health);
+        System.out.println("Fighter1 health " + fighter1.health);
+
+        return "name";
     }
 
 }
