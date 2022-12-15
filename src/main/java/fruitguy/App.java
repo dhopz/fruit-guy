458693ded7387ba23353e5385e2a8463b649989b
dhopz <david_houpapa@hotmail.com>
2022-12-15 16:19:28 +0000

    removing highest and lowest

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -3,13 +3,16 @@ package fruitguy;
 
 import java.util.*;
 
+import static fruitguy.Katas.removeLowestHighets.sum;
 
 
 public class App {
     public static void main(String[] args) {
+        System.out.println(sum(new int[]{ }));
 
 
     }
+
 }
 
 
