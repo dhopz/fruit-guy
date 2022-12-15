1ba4fd98cefb04521c99170ce2dc0b7e5cab9b56
dhopz <david_houpapa@hotmail.com>
2022-12-15 15:44:00 +0000

    fuel price

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -1,25 +1,13 @@
 package fruitguy;
 
-import fruitguy.Katas.DecWinner.Fighter;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.*;
 
-import static fruitguy.Katas.DecWinner.declareWinner;
-import static fruitguy.Katas.DescendingOrderDigit.sortDesc;
-import static fruitguy.Katas.DriversLicence.driver;
 
 
 public class App {
     public static void main(String[] args) {
-//        Fighter fighter1 = new Fighter("Lew",10,2);
-//        Fighter fighter2 = new Fighter("Harry",5,4);
-//        System.out.println(declareWinner(fighter1,fighter2,"Lew"));
-//        System.out.println(declareWinner(new Fighter("Lew", 10, 2),new Fighter("Harry", 5, 4), "Harry"));
-        System.out.println(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry"));
-//        System.out.println(declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald"));
-//        System.out.println(declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Harald"));
+
 
     }
 }
