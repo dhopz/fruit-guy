0eb22968c9c0207a9662c2ac51d716bcf4572eca
dhopz <david_houpapa@hotmail.com>
2023-01-05 16:49:59 +0000

    cleaning pipefix

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -3,42 +3,18 @@ package fruitguy;
 
 import java.util.*;
 
-import static fruitguy.Katas.removeLowestHighets.sum;
+import static fruitguy.Katas.FixPipe.pipeFix;
 
 
 public class App {
     public static void main(String[] args) {
-//        System.out.println(Arrays.toString(pipeFix(new int[]{1, 2, 3, 5, 6, 8, 9})));
-//        System.out.println(Arrays.toString(pipeFix(new int[]{6, 9})));
-        System.out.println(tripleTrouble("this","test","lock"));
-        System.out.println(tripleTrouble("aa","bb","cc"));
+        System.out.println(Arrays.toString(pipeFix(new int[]{1, 2, 3, 5, 6, 8, 9})));
+        System.out.println(Arrays.toString(pipeFix(new int[]{6, 9})));
 
 
     }
 
-    public static int[] pipeFix(int[] numbers) {
-        int[] result = new int[numbers[numbers.length-1] + 1 - numbers[0]];
-        result[0] = numbers[0];
 
-        for (int a = 1; a<(numbers[numbers.length-1] + 1 - numbers[0]); a++) {
-            result[a] = result[a-1] + 1;
-        }
-
-        return result;
-    }
-
-    public static String tripleTrouble(String one, String two, String three) {
-        StringBuilder newString = new StringBuilder();
-
-        for(int i = 0; i < one.length(); i++){
-            newString.append(one.toCharArray()[i]);
-            newString.append(two.toCharArray()[i]);
-            newString.append(three.toCharArray()[i]);
-        }
-
-
-        return newString.toString();
-    }
 
 
 
---
+++ src/main/java/fruitguy/Katas/TripTrub.java
@@ -0,0 +1,16 @@
+package fruitguy.Katas;
+
+public class TripTrub {
+    public static String tripleTrouble(String one, String two, String three) {
+        StringBuilder newString = new StringBuilder();
+
+        for(int i = 0; i < one.length(); i++){
+            newString.append(one.toCharArray()[i]);
+            newString.append(two.toCharArray()[i]);
+            newString.append(three.toCharArray()[i]);
+        }
+
+
+        return newString.toString();
+    }
+}
