31aa5b6be901b4046edcb283b0a8be46d0c238e5
dhopz <david_houpapa@hotmail.com>
2023-01-06 11:24:57 +0000

    adding solution

--- src/main/java/fruitguy/Katas/CountPassengers.java
+++ src/main/java/fruitguy/Katas/CountPassengers.java
@@ -3,6 +3,7 @@ package fruitguy.Katas;
 import java.util.ArrayList;
 
 public class CountPassengers {
+    // https://www.codewars.com/kata/5648b12ce68d9daa6b000099/train/java
     public static int countPassengers(ArrayList<int[]> stops) {
         int peopleOn = 0;
         int peopleOff = 0;
