ae3529cf37313a8abc6f1035d7294b588d26b16f
dhopz <david_houpapa@hotmail.com>
2023-01-06 11:44:28 +0000

    nerdify

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -2,18 +2,15 @@ package fruitguy;
 import java.util.*;
 
 import static fruitguy.Katas.CountPassengers.countPassengers;
+import static fruitguy.Katas.Nerdify.nerdify;
 
 public class App {
     public static void main(String[] args) {
-        ArrayList<int[]> list = new ArrayList<int[]>();
-        list.add(new int[] {10,0});
-        list.add(new int[] {3,5});
-        list.add(new int[] {2,5});
-
-        System.out.println(countPassengers(list));
+        System.out.println(nerdify("Los Angeles"));
 
 
     }
+
 
 
 
---
+++ src/main/java/fruitguy/Katas/Nerdify.java
@@ -0,0 +1,34 @@
+package fruitguy.Katas;
+
+import java.util.ArrayList;
+
+public class Nerdify {
+    //https://www.codewars.com/kata/59e9f404fc3c49ab24000112/java
+    public static String bestNerdify(String txt){
+        return txt.replaceAll("[aA]", "4").replaceAll("[eE]", "3").replace("l", "1");
+    }
+    public static String nerdify(String txt){
+        char[] ch = txt.toCharArray();
+        ArrayList<Character> newTxt = new ArrayList();
+        StringBuilder builder = new StringBuilder(newTxt.size());
+
+        for (char c : ch) {
+            if (c == 'e' || c == 'E') {
+                newTxt.add('3');
+            } else if (c == 'a' || c == 'A'){
+                newTxt.add('4');
+            } else if (c == 'l') {
+                newTxt.add('1');
+            } else {
+                newTxt.add(c);
+            }
+        }
+
+        for(Character x: newTxt)
+        {
+            builder.append(x);
+        }
+
+        return builder.toString();
+    }
+}
