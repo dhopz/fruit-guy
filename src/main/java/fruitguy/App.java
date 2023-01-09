70457e9b5cb6ccb4b42bad5b252baf3a1ea04b30
dhopz <david_houpapa@hotmail.com>
2023-01-09 16:18:50 +0000

    replace every nth

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 import static fruitguy.Katas.CountPassengers.countPassengers;
 import static fruitguy.Katas.Nerdify.nerdify;
+import static fruitguy.Katas.ReplaceNth.replaceNth;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(nerdify("Los Angeles"));
+        System.out.println(replaceNth("Vader said: No, I am your father!", 1, 'i', 'y'));
 
 
     }
-
 
 
 
