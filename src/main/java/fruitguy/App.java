f2c009ba1733b09af3a108885ca845f60b2fadd4
dhopz <david_houpapa@hotmail.com>
2023-01-20 16:00:48 +0000

    working on insertdash

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -1,13 +1,38 @@
 package fruitguy;
 
+import java.util.*;
+
 import static fruitguy.Katas.Strings.FilterString.filterString;
 
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(filterString("aa1bb2cc3dd"));
-        System.out.println(filterString("1766903424209212178"));
+        System.out.println(insertDash(454793));
 
+    }
+    //https://www.codewars.com/kata/55960bbb182094bc4800007b/train/java
+
+    public static String insertDash(int i) {;
+        List<Integer> digits = new ArrayList<>();
+        List<Integer> newDigits = new ArrayList<>();
+
+
+        while(i > 0) {
+            digits.add(i % 10);
+            i /= 10;
+        }
+        System.out.println(digits);
+
+        newDigits.add(digits.get(0));
+
+        for(int x = 1; i<digits.size(); i++){
+            if(newDigits.get(0) % 2 == 0){
+                newDigits.add(digits.get(x));
+            }
+            System.out.println(newDigits);
+        }
+
+        return "";
     }
 
 
