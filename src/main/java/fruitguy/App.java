ef6ef685e4776a77872e9c7139eeb1906cf4b407
dhopz <david_houpapa@hotmail.com>
2023-01-20 11:06:43 +0000

    covfefe

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -2,7 +2,7 @@ package fruitguy;
 import java.util.*;
 import java.lang.Math;
 
-import static fruitguy.Katas.Bingo.bingo;
+import static fruitguy.Katas.Covfefe.covfefe;
 
 
 public class App {
@@ -12,25 +12,6 @@ public class App {
         System.out.println(covfefe("nothing"));
         System.out.println(covfefe("covfefe"));
         System.out.println(covfefe("double space "));
-
-    }
-
-    public static String covfefe(String tweet) {
-        String[] words = tweet.split(" ");
-        System.out.println(Arrays.toString(words));
-        int counter = 0;
-        for(int i = 0; i < words.length; i++) {
-            if (Objects.equals(words[i], "coverage")) {
-                counter++;
-            }
-            words[i] = words[i].replace("coverage", "covfefe");
-        }
-//        if (counter == 0){
-//            return String.join(" ", words);
-//        } else if (words.length > 1) {
-//            return String.
-//        }
-        return counter == 0 ? String.join(" ", words) + " covfefe" : String.join(" ", words);
 
     }
 
