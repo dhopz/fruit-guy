4e9b23b392d6622dee12195057ff04b666b59955
dhopz <david_houpapa@hotmail.com>
2023-04-13 15:50:30 +0100

    persistence

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -6,7 +6,7 @@ import static fruitguy.Katas.Integers.DigitalRoot.digital_root;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(digital_root(942));
+        System.out.println(digital_root(25));
     }
 
 
--- src/main/java/fruitguy/Katas/Integers/DigitalRoot.java
+++ src/main/java/fruitguy/Katas/Integers/DigitalRoot.java
@@ -2,15 +2,18 @@ package fruitguy.Katas.Integers;
 
 public class DigitalRoot {
     //https://www.codewars.com/kata/541c8630095125aba6000c00/java
-    public static int digital_root(int n) {
+    //https://www.codewars.com/kata/55bf01e5a717a0d57e0000ec/java
+    public static int digital_root(long n) {
+        int persistence = 0;
         while(n > 9) {
-            int sum = 0;
+            int sum = 1;
             while (n > 0) {
-                sum += n % 10;
+                sum *= n % 10;
                 n /= 10;
             }
             n = sum;
+            persistence ++;
         }
-        return n;
+        return (int) persistence;
     }
 }
