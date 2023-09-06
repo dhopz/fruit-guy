a8a505021acfa54933fc1c0ed5af0003e27ebc5c
dhopz <david_houpapa@hotmail.com>
2023-09-06 15:40:07 +0100

    rusty mcrust rust

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -1,11 +1,32 @@
 package fruitguy;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static fruitguy.Katas.Integers.DutyFree.dutyFree;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(dutyFree(17,10,500));
+        String[] newWords = new String[]{"kevin", "has", "no", "space"};
+        System.out.println(Arrays.toString(spacey(newWords)));
 
+    }
+
+    public static String[] spacey(String[] array) {
+        int counter = 0;
+        List<String> combinedString = new ArrayList<>();
+        combinedString.add(array[0]);
+        for (String x: array){
+            if (counter == 0){
+                combinedString.add(array[0]);
+            } else {
+                combinedString.add(combinedString.get(counter) + x);
+            }
+            counter ++;
+        }
+        combinedString.remove(0);
+        return combinedString.toArray(new String[0]);
     }
 
 
