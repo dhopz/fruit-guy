0259c4934c050f56de1a77fb03bb8a39462d5d3d
dhopz <david_houpapa@hotmail.com>
2023-09-07 11:25:48 +0100

    little bit per day

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -6,8 +6,43 @@ import static fruitguy.Katas.Arrays.Spacey.spacey;
 
 public class App {
     public static void main(String[] args) {
-        String[] newWords = new String[]{"kevin", "has", "no", "space"};
-        System.out.println(Arrays.toString(spacey(newWords)));
+        System.out.println(ATM(770));
+        System.out.println(ATM(238));
+
+    }
+
+    public static int ATM(int n) {
+//        int i = 0;
+//        while (i < 5) {
+//            System.out.println(i);
+//            i++;
+//        }
+
+//        if(n % 500 == 0){
+//            System.out.println("this ");
+//            counter ++;
+//        } else {
+//            System.out.println("divisible");
+//            counter ++;
+//        }
+//        System.out.println(n % 500);
+
+        // Variable to store the counter
+        int counter = 0;
+
+        // Running a loop from A to B and check
+        // if a number is divisible by M.
+        int newNumber = n;
+        for (int i = 0; i <= n; i++)
+            if (i % 500 == 0)
+                newNumber = newNumber - 500;
+                counter++;
+
+        return counter;
+
+
+
+
     }
 
 
