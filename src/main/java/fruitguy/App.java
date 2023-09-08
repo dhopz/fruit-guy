969313f7a780a8880f41341589a808cb1290a3de
dhopz <david_houpapa@hotmail.com>
2023-09-08 11:06:48 +0100

    working on arrays

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -1,13 +1,34 @@
 package fruitguy;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 import static fruitguy.Katas.WhileLoop.Atm.solution;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(solution(10));
+        String[] cards = new String []{"3", "9", "A", "5", "T", "8", "2", "4", "Q", "7", "J", "6", "K"};
+        System.out.println(Arrays.toString(sortCards(cards)));
 
   }
+
+    public static String[] sortCards (String[] cards) {
+        List<String> sortedList = new ArrayList<>();
+        String[] ranked = new String[]{"A", "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K"};
+        for (String card : cards) {
+            System.out.println(card);
+        }
+
+        for (int i = 0; i < cards.length; i++) {
+            if (Objects.equals(cards[i], ranked[i])) {
+                System.out.println("here");
+            }
+        }
+        return new String[]{"a","b","c"};
+    }
 
 
 
