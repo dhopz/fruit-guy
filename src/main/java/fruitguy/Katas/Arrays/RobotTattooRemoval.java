1dfd73bee498a5584274c8ff35f96d75c7d0dd34
dhopz <david_houpapa@hotmail.com>
2023-09-12 11:18:00 +0100

    circle area

--- src/main/java/fruitguy/Katas/Arrays/RobotTattooRemoval.java
+++ src/main/java/fruitguy/Katas/Arrays/RobotTattooRemoval.java
@@ -3,6 +3,8 @@ package fruitguy.Katas.Arrays;
 import java.util.Objects;
 
 public class RobotTattooRemoval {
+    //https://www.codewars.com/kata/57658f3dedc6f7a751000e7b/java
+
     public static String[][] robot(String[][] skinScan) {
         for(String[] skin:skinScan){
             for(int i = 0; i < skin.length; i++){
