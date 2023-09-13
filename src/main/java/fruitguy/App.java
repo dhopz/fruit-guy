2daf81b5626a774e1aafea2bda0cf5dc526d9a27
dhopz <david_houpapa@hotmail.com>
2023-09-13 12:10:51 +0100

    starting shifteddiff

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -1,13 +1,25 @@
 package fruitguy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static fruitguy.Katas.Doubles.Circle.area;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println(area(68));
-        System.out.println(area(-1));
+        System.out.println(shiftedDiff("coffee","eecoff"));
+        System.out.println(shiftedDiff("hoop","pooh"));
 
   }
+
+    static int shiftedDiff(String first, String second){
+        List<Character> chars = new ArrayList<>();
+        for (char ch : first.toCharArray()) {
+            chars.add(ch);
+        }
+        System.out.println(chars);
+        return -1;
+    }
 
 
 
