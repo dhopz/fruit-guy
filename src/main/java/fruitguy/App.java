d8bdba887e9bb2a1c198f1da35791f4c12122363
dhopz <david_houpapa@hotmail.com>
2023-09-14 17:04:45 +0100

    not working for one condition

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -9,6 +9,8 @@ import static fruitguy.Katas.Doubles.Circle.area;
 public class App {
     public static void main(String[] args) {
         System.out.println(shiftedDiff("coffee","eecoff"));
+        System.out.println(shiftedDiff("eecoff","coffee"));
+
         System.out.println(shiftedDiff("hoop","pooh"));
 
   }
@@ -16,36 +18,22 @@ public class App {
   //https://www.codewars.com/kata/5596f6e9529e9ab6fb000014/train/java
   static int shiftedDiff(String first, String second){
       List<String> myList = new ArrayList<>(Arrays.asList(first.split("")));
-      System.out.println(myList);
-      myList.add(0, myList.remove(myList.size() - 1));
-      System.out.println(myList);
+      boolean match = false;
+      int i = 1;
+      while (i < first.length()) {
+          myList.add(0, myList.remove(myList.size() - 1));
+          StringBuilder builder = new StringBuilder(myList.size());
+          for(String ch: myList){
+              builder.append(ch);
+          }
+          if(builder.toString().equals(second)){
+              match = true;
+              break;
+          }
+          i++;
+      }
 
-
-      List<Character> chars = new ArrayList<>();
-        List<Character> secondChars = new ArrayList<>();
-
-        for (char ch : first.toCharArray()) {
-            chars.add(ch);
-        }
-
-        StringBuilder builder = new StringBuilder(chars.size());
-        for(Character ch: chars)
-        {
-            builder.append(ch);
-        }
-        System.out.println(builder.toString());
-
-      for (int i = 0; i < chars.size()-1; i++)
-          chars.set(i, chars.get(i + 1));
-
-      System.out.println(chars);
-
-        for (char ch : second.toCharArray()) {
-            secondChars.add(ch);
-        }
-        System.out.println(chars);
-
-        return -1;
+      return match ? i : -1;
     }
 
 
