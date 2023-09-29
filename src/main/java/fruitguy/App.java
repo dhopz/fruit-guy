59464eaa3c8971c68602a842eb99b1d80387dfb0
dhopz <david_houpapa@hotmail.com>
2023-09-29 16:49:39 +0100

    arraycopy is causing loop error timeout

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -10,9 +10,9 @@ import java.util.List;
 public class App {
     public static void main(String[] args) {
         int[] parts = new int[] {0, 1, 3, 6, 10};
+        int[] newParts = new int[] {744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358};
         System.out.println(Arrays.toString(sumParts(parts)));
-//        System.out.println(Arrays.toString(removeElement(parts, 3)));
-        System.out.println(calculateSum(parts));
+        System.out.println(Arrays.toString(sumParts(newParts)));
 
 
   }
@@ -29,52 +29,35 @@ public class App {
         return arr_new;
     }
 
-    public static int calculateSum(int[] ls){
+    public static int calculateSum(int[] ls, int arrLength){
         int total = 0;
-        for (int n : ls) {
+        int[] newArr = new int[ls.length];
+        System.arraycopy(ls, arrLength, newArr, 0, ls.length - arrLength);
+
+        for (int n : newArr) {
             total += n;
         }
         return total;
     }
 
+
     public static int[] sumParts(int[] ls) {
         //https://www.codewars.com/kata/5ce399e0047a45001c853c2b/train/java
-        int[] sums = new int[ls.length];
+        int[] sums = new int[ls.length+1];
 
         int i = 0;
-//        int total = 0;
         for (int sum : sums) {
             int total = 0;
             if(i==0){
-                System.out.println("this is i");
                 for (int n : ls) {
                     total += n;
                 }
-//                sums[i++] = total;
-                System.out.println(total);
 
             } else {
-                System.out.println("not here");
-                int[] newArr = new int[ls.length];
-                System.arraycopy(ls, i, newArr, 0, ls.length - i);
-                System.out.println(Arrays.toString(newArr));
-
-                for (int n : newArr) {
-                    total += n;
-                }
-                System.out.println(total);
+                total = calculateSum(ls,i);
             }
+            System.out.println(total);
 
-
-
-//            int[] newArr = new int[ls.length];
-//            System.arraycopy(ls, i, newArr, 0, ls.length - i);
-//            System.out.println(Arrays.toString(newArr));
-//
-//            for (int n : newArr) {
-//                total += n;
-//            }
-//            System.out.println(total);
             sums[i++] = total;
 
         }
