b25c9d3b8e8651f860bdf19eeeebdf6f6ae645cd
dhopz <david_houpapa@hotmail.com>
2023-10-02 17:06:20 +0100

    might have to skip this one

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -7,13 +7,13 @@ import static fruitguy.Katas.Strings.LongWord.longestWord;
 public class App {
     public static void main(String[] args) {
 //        System.out.println(nextSmallerInteger(21));
-        int num = 2071;
-        int result = nextSmallerInteger(num);
+        int num = 907;
+        int result = nextSmallestInteger(num);
         System.out.println("Next smaller integer with the same digits as " + num + " is: " + result);
 
 
     }
-    public static int nextSmallerInteger(long n) {
+    public static int nextSmallerIntegerTry(long n) {
         //https://www.codewars.com/kata/5659c6d896bc135c4c00021e/train/java
         int num = (int) n;
         String numStr = Integer.toString(num);
@@ -35,48 +35,43 @@ public class App {
         }
     }
 
-    public static int nextSmallerIntegerTry(int num) {
-        // Convert the integer to a string to work with its digits
-        String numStr = Integer.toString(num);
-        char[] digits = numStr.toCharArray();
+        public static int nextSmallestInteger(int num) {
+            // Convert the integer to a string for easier manipulation
+            String numStr = Integer.toString(num);
+            char[] digits = numStr.toCharArray();
 
-        // Step 1: Find the first pair of adjacent digits where the left digit is smaller than the right digit
-        int i = digits.length - 2;
-        while (i >= 0 && digits[i] >= digits[i + 1]) {
-            i--;
+            // Find the first pair of adjacent digits where the left digit is greater than the right digit
+            int i = digits.length - 2;
+            while (i >= 0 && digits[i] <= digits[i + 1]) {
+                i--;
+            }
+
+            // If no such pair is found, it's not possible to find a smaller integer
+            if (i == -1) {
+                return -1; // or any other value to indicate that no smaller integer exists
+            }
+
+            // Find the largest digit to the right of digits[i] that is smaller than digits[i]
+            int j = digits.length - 1;
+            while (digits[j] >= digits[i]) {
+                j--;
+            }
+
+            // Swap the two digits
+            char temp = digits[i];
+            digits[i] = digits[j];
+            digits[j] = temp;
+
+            // Sort all the digits to the right of digits[i] in descending order
+            Arrays.sort(digits, i + 1, digits.length);
+
+            // Convert the modified string back to an integer and return it
+            return Integer.parseInt(new String(digits));
         }
 
-        // If no such pair is found, the number is already the smallest possible
-        if (i == -1) {
-            return num;
-        }
-
-        // Step 2: Find the smallest digit to the right of 'i' that is greater than digits[i]
-        int j = digits.length - 1;
-        while (digits[j] <= digits[i]) {
-            j--;
-        }
-
-        // Step 3: Swap digits[i] and digits[j]
-        char temp = digits[i];
-        digits[i] = digits[j];
-        digits[j] = temp;
-
-        // Step 4: Sort all digits to the right of digits[i] in ascending order
-        Arrays.sort(digits, i + 1, digits.length);
-
-//        Arrays.sort(digits, i + 1, digits.length);
-//        for (int start = i + 1, end = digits.length - 1; start < end; start++, end--) {
-//            char temp2 = digits[start];
-//            digits[start] = digits[end];
-//            digits[end] = temp2;
-//        }
-
-        // Step 5: Convert the modified string back to an integer and return it
-        return Integer.parseInt(new String(digits));
     }
 
-}
+
 
 
 
