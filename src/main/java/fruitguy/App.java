b65f00046a3a909eb84ad5835afe1ce20077cd68
dhopz <david_houpapa@hotmail.com>
2023-10-02 15:14:00 +0100

    longest word

--- src/main/java/fruitguy/App.java
+++ src/main/java/fruitguy/App.java
@@ -8,14 +8,20 @@ import static fruitguy.Katas.Arrays.SumParts.partsSums;
 
 public class App {
     public static void main(String[] args) {
-        int[] parts = new int[] {0, 1, 3, 6, 10};
-        int[] newParts = new int[] {744125, 935, 407, 454, 430, 90, 144, 6710213, 889, 810, 2579358};
-
-        System.out.println(Arrays.toString(partsSums(parts)));
-        System.out.println(Arrays.toString(partsSums(newParts)));
-
-
+        System.out.println(longestWord("one two three"));
   }
+
+    public static String longestWord(String wordString) {
+        int wordLength = 0;
+        String longestWord = null;
+        for(String word: wordString.split(" ")){
+            if(word.length() >= wordLength){
+                wordLength = word.length();
+                longestWord = word;
+            }
+        }
+        return longestWord;
+    }
 
 
 
